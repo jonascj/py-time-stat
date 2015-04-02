01930c763f9c61c0d69fa97c298b25b3fbe8035e
Jonas Camillus Jeppesen <wuhtzu@gmail.com>
2015-04-02 14:00:21 +0200

    Added output of statistics

--- py-time-stat.py
+++ py-time-stat.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python 
 import argparse
 import subprocess
 import os
 import tempfile
+from numpy import std, mean
+from math import sqrt
 
 # Arguments
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -23,6 +26,8 @@ args = parser.parse_args()
 fi, fpath = tempfile.mkstemp(suffix='.out', prefix='py-time-stat-', dir='/tmp')
 
 # Command to run
+# %realtime;%usercpu;%kernelcpu
+# http://man7.org/linux/man-pages/man1/time.1.html
 cmd = '/usr/bin/time -f "%e;%U;%S" -a -o {} {}'.format(fpath, args.command)
 
 # stdout and stderr
@@ -52,5 +57,28 @@ for i in range(1,args.runs+1):
     if args.stderr:
         print "STDERR:\n{}".format(p.stderr.read())
 
-
 devnull.close()
+
+# Parse the file outputted by /usr/bin/time
+with open(fpath) as f:
+    lines = f.readlines()
+
+treal = []
+tuser = []
+tkernel = []
+
+for l in lines:
+    tr, tu, tk = [float(x) for x in l.split(';')]
+    treal.append(tr)
+    tuser.append(tu)
+    tkernel.append(tk)
+
+os.remove(fpath)
+
+# Output statistics
+print "Real:\t{:.4E} +/- {:.4E} s".format(mean(treal), std(treal)/sqrt(len(treal)))
+print "User:\t{:.4E} +/- {:.4E} s".format(mean(tuser), std(tuser)/sqrt(len(tuser)))
+print "Kernel:\t{:.4E} +/- {:.4E} s".format(mean(tkernel), std(tkernel)/sqrt(len(tkernel)))
+
+
+
